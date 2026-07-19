46e566f17ff937959c1faaf397df04ba70e71773
R G Yasindu sewmith <as20240555@sci.sjp.ac.lk>
2026-07-19 18:26:08 +0530

    add_backedn_JavaProject

---
+++ Backend/Statrics/src/main/java/org/example/statrics/StatricsApplication.java
@@ -0,0 +1,13 @@
+package org.example.statrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatricsApplication.class, args);
+    }
+
+}
---
+++ Backend/Statrics/src/test/java/org/example/statrics/StatricsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.statrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
