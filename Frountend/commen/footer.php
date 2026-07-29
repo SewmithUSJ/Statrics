1a283849de60ef83e85c6c60e502bdf0d0209dc7
R G Yasindu sewmith <as20240555@sci.sjp.ac.lk>
2026-07-29 13:39:49 +0530

    Revert "connect_userdashboad(home&projectRequest)"
    This reverts commit b45f72b7948c4c502a39193269d1eda4f924207d.

--- Frountend/commen/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-            <footer>
-                <p>&copy; <?php echo date("Y"); ?> StatRics Consultancy. All rights reserved.</p>
-            </footer>
-        </div>
-    </div>
-    <script src="script.js"></script>
-</body>
-</html>
